09a195c04031e6b0e0e66baf83bcc1fcfa3d8d31
heitorgusan <heitorgusan@gmail.com>
2022-04-16 21:12:41 -0300

    Primeiro commit: configurações e Model:Postagem

---
+++ meublog/src/main/java/com/meublog/meublog/MeublogApplication.java
@@ -0,0 +1,13 @@
+package com.meublog.meublog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeublogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeublogApplication.class, args);
+	}
+
+}
